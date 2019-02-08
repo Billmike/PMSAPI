d95730d077f8eee0f39e5813514a60186eee2800
Ayelegun Kayode Michael <billmike1994@gmail.com>
2019-02-08 19:28:50 +0100

    Implement route for update and deleting locations

--- server/controllers/location.controller.js
+++ server/controllers/location.controller.js
@@ -88,3 +88,108 @@ exports.fetchOneLocation = (request, response) => {
       })
     })
 }
+
+exports.fetchAllLocations = (request, response) => {
+  return Location.all({
+    include: [
+      { model: LocationDetails, as: 'locationDetails', nested: 'true' }
+    ]
+  }).then(locations => {
+    return response.status(200).json({
+      message: 'Retrieved all locations',
+      locations
+    })
+  }).catch(error => {
+    return response.status(500).json({
+      message: 'An error occurred'
+    })
+  })
+}
+
+exports.updateLocation = (request, response) => {
+  if (isNaN(Number(request.params.locationId))) {
+    return response.status(400).json({
+      message: 'Invalid locationID'
+    })
+  }
+
+  if (request.body.parentLocationId) {
+    return Location.findById(Number(request.body.parentLocationId)).then(foundLocation => {
+      if (!foundLocation) {
+        return response.status(404).json({
+          message: 'No parent location with that ID found'
+        })
+      }
+
+      return LocationDetails.find({
+        where: {
+          locationId: request.params.locationId
+        },
+        include: [
+          { model: Location, as: 'locationDetails' }
+        ]
+      }).then(locationDetail => {
+        if (!locationDetail) {
+          return response.status(404).json({
+            message: 'No location detail found'
+          })
+        }
+        const location = locationDetail.dataValues.locationDetails
+        console.log(location.dataValues.name);
+        const name = request.body.name || location.dataValues.name
+        const femalePopulation = request.body.femalePopulation || locationDetail.femalePopulation
+        const malePopulation = request.body.malePopulation || locationDetail.malePopulation
+        const sumPopulation = Number(femalePopulation) + Number(malePopulation)
+        const parentLocationId = request.body.parentLocationId || locationDetail.parentLocation
+
+        location.set('name', name)
+        location.save()
+
+        locationDetail.set('femalePopulation', femalePopulation)
+        locationDetail.set('malePopulation', malePopulation)
+        locationDetail.set('sumPopulation', sumPopulation)
+        locationDetail.set('parentLocation', parentLocationId)
+        locationDetail.save()
+
+        return response.status(200).json({
+          status: 'Success',
+          message: 'Location status updated successfully',
+          locationDetail
+        })
+      })
+    }).catch(error => {
+      return response.status(500).json({
+        message: 'An error occurred'
+      })
+    })
+  }
+}
+
+exports.deleteLocation = (request, response) => {
+  if (isNaN(Number(request.params.locationId))) {
+    return response.status(400).json({
+      message: 'LocationID invalid'
+    })
+  }
+
+  return Location.findById(Number(request.params.locationId)).then(locationDetail => {
+    if (!locationDetail) {
+      return response.status(404).json({
+        status: '404 Error',
+        message: 'No location with that ID found'
+      })
+    }
+
+    return locationDetail.destroy().then(() => {
+      return response.status(200).json({
+        status: 'Success',
+        message: 'Location deleted successfully'
+      })
+    })
+  }).catch(error => {
+    return response.status(500).json({
+      status: '500 error',
+      message: 'An error occurred'
+    })
+  })
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,7 @@ const locationController = require('../controllers/location.controller');
 module.exports = (app) => {
   app.post('/api/location', locationController.create);
   app.get('/api/location/:locationId', locationController.fetchOneLocation);
+  app.get('/api/locations', locationController.fetchAllLocations)
+  app.put('/api/location/:locationId', locationController.updateLocation)
+  app.delete('/api/location/:locationId', locationController.deleteLocation)
 }
